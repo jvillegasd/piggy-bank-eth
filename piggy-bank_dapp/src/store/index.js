569a9a5656ee15c1f325d0489636e0929ac7781a
C9-LinkRs <linkrsfan@gmail.com>
2021-05-08 01:34:04 +0200

    vuex and store set up

---
+++ piggy-bank_dapp/src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import state from './state.js';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  strict: true,
+  state,
+  mutations: {},
+  actions: {}
+});

ace373417d161522d07b7ae18684a1e9d5986395
C9-LinkRs <linkrsfan@gmail.com>
2021-05-08 02:42:08 +0200

    web3/metamask implementation almost done

---
+++ piggy-bank_dapp/src/utils/getWeb3.js
@@ -0,0 +1,43 @@
+import Web3 from 'web3';
+
+let getWeb3 = new Promise((resolve, reject) => {
+
+  // Check if web3 is injected (metamask activated)
+  let web3js = window.web3;
+  if (typeof web3js !== 'undefined') {
+    let web3 = new Web3(web3js.currentProvider);
+    resolve({
+      injectedWeb3: web3.isConnected(),
+      web3 () {
+        return web3;
+      }
+    });
+  } else {
+    reject(new Error('Unable to connect to Metamask'));
+  }
+
+}).then(result => {
+  return new Promise((resolve, reject) => {
+    // Retrieve network ID
+    result.web3().version.getNetwork((err, networkId) => {
+      if (err) {
+        reject(new Error('Unable to retrieve network ID'));
+      } else {
+        result = Object.assign({}, result, { networkId });
+        resolve(result);
+      }
+    });
+  });
+}).then(result => {
+  return new Promise((resolve, reject) => {
+    // Retrieve balance for coinbase
+    result.web3().eth.getBalance(result.coinbase, (err, balance) => {
+      if (err) {
+        reject(new Error('Unable to retrieve balance for address: ' + result.coinbase));
+      } else {
+        result = Object.assign({}, result, { balance });
+        resolve(result);
+      }
+    });
+  });
+});

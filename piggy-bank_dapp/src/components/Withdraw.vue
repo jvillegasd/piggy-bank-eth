1f815932a75ca0d26795d9d2c19306cd215bcf36
C9-LinkRs <linkrsfan@gmail.com>
2021-05-08 11:18:10 +0200

    withdraw component done

--- piggy-bank_dapp/src/components/Withdraw.vue
+++ piggy-bank_dapp/src/components/Withdraw.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <label for="withdraw">Withdraw amount</label>
-    <input type="text" id="withdraw" name="withdraw" v-model="withdrawAmount">
-    <button @click=withdrawEther>Withdraw</button>
+  <div class="withdraw_card">
+    <label class="withdraw_label" for="withdraw">Withdraw amount</label>
+    <input class="withdraw_input" type="text" id="withdraw" name="withdraw" v-model="withdrawAmount" placeholder="An ether amount">
+    <button class="withdraw_bt" @click=withdrawEther>Withdraw</button>
   </div>
 </template>
 
@@ -11,11 +11,16 @@ export default {
   name: 'withdraw',
   data () {
     return {
-      withdrawAmount: 0
+      withdrawAmount: null
     }
   },
   methods: {
     withdrawEther () {
+      if (this.withdrawAmount === undefined || this.withdrawAmount === null) {
+        window.alert('Digit a valid Ether amount')
+        return
+      }
+
       const { web3, contractInstance } = this.$store.state
       contractInstance.methods.withdraw(this.withdrawAmount).send({
         from: web3.coinbase
@@ -25,6 +30,41 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .withdraw_card {
+    border-radius: 15px;
+    box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
+    width: 200px;
+    height: 100px;
 
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .withdraw_bt {
+    background-color: rgba(0, 94, 238, 0.8);
+    outline: none;
+    border: none;
+    width: 75px;
+    height: 30px;
+    color: white;
+    border-radius: 10px;
+  }
+  .withdraw_bt:hover {
+    background-color: white;
+    color: rgba(0, 94, 238, 0.8);
+    border-style: solid;
+    border-width: 2px;
+    border-color: rgba(0, 94, 238, 0.8);
+  }
+  .withdraw_label {
+    font-weight: bold;
+  }
+  .withdraw_input {
+    border-radius: 7px;
+    border-color: gray;
+    border-style: solid;
+    outline: none;
+  }
 </style>
